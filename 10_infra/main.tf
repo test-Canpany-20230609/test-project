80ba718e364757c8607cc3c94afffa771b84e5da
kazuya otsuka <kazuya0412ka@gmail.com>
2023-06-09 14:18:09 +0900

    s3バケット名を追加

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -10,8 +10,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "{YOUR-S3-BUCKET-NAME}"
-    key     = "{YOUR-S3-BUCKET-KEY}"
+    bucket  = "terraform-test-backet-20230605"
+    key     = "github-action.tfstate"
     region  = "ap-northeast-1"
     profile = "terraform"
   }
