365f642e3259a1aea31feb2f6f4a6ed7ca61fe4d
kazuya otsuka <kazuya0412ka@gmail.com>
2023-06-11 18:18:03 +0900

    format repier

--- 10_infra/network.tf
+++ 10_infra/network.tf
@@ -3,13 +3,13 @@
 # ---------------------------------------------
 resource "aws_vpc" "vpc" {
   cidr_block                       = "192.168.0.0/20"
-  instance_tenancy           = "default"
-  enable_dns_support           = true
+  instance_tenancy                 = "default"
+  enable_dns_support               = true
   enable_dns_hostnames             = true
   assign_generated_ipv6_cidr_block = false
 
   tags = {
-    Name            = "${var.project}-${var.environment}-vpc"
+    Name    = "${var.project}-${var.environment}-vpc"
     Project = var.project
     Env     = var.environment
   }
